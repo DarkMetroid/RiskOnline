2fbdc9d1f403686c8dc11bec262ff9187cb9941a
Jerry Seto <jerryseto1@gmail.com>
2012-02-25 15:24:47 -0500

    Switched to rails

---
+++ Risk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Risk::Application.config.secret_token = '6faa9df729b3de6ba91e58ddc6b35c07d4ba1ad53103b2c5b1f1fb24580e1a452f9f46ed153859fa57865815cf67a25e87082b656cffb2eefbbd5bc193a47d6f'
