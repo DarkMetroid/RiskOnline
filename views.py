d15ef0e8a6bddce9cd1d9864b06e0924cbaa82a8
Jerry Seto <jerryseto1@gmail.com>
2012-02-25 13:49:45 -0500

    deleted everything

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.core.context_processors import csrf
-from django.http import HttpResponse
-from django.template import RequestContext
-
-def homepage(request): 
-	rc = RequestContext(request, {'STATIC_URL' : '/statics/',})
-	return render_to_response('login.html', rc)
